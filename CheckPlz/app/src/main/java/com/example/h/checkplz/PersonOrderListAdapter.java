7d0bd552fe35798ca870cd78dcb83b2ff81babdd
Jack <gungravefire@yahoo.com>
2018-12-31 12:21:44 -0800

    Adapter update

--- CheckPlz/app/src/main/java/com/example/h/checkplz/PersonOrderListAdapter.java
+++ CheckPlz/app/src/main/java/com/example/h/checkplz/PersonOrderListAdapter.java
@@ -1,4 +1,32 @@
 package com.example.h.checkplz;
 
-public class PersonOrderListAdapter {
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class PersonOrderListAdapter extends
+        RecyclerView.Adapter<PersonOrderListAdapter.ViewHolder>{
+
+    // Provide a direct reference to each of the views within a data item
+    // Used to cache the views within the item layout for fast access
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        // Your holder should contain a member variable
+        // for any view that will be set as you render a row
+        public EditText personOrderNameEditText;
+        public EditText personOrderCostEditText;
+        public TextView personOrderMultipleSymbolTextView;
+        public EditText personOrderMultipleAmountEditText;
+
+        // We also create a constructor that accepts the entire item row
+        // and does the view lookups to find each subview
+        public ViewHolder(View itemView){
+            // Stores the itemView in a public final member variable that can be used
+            // to access the context from any ViewHolder instance.
+            super(itemView);
+
+
+        }
+    }
+
 }
