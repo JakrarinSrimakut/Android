353e3201b99419e3fe91139d4b1b17be7247bcab
Jack <gungravefire@yahoo.com>
2019-02-06 20:41:12 -0600

    PersonOrderListAdapter updated

--- CheckPlz/app/src/main/java/com/example/h/checkplz/PersonOrderListAdapter.java
+++ CheckPlz/app/src/main/java/com/example/h/checkplz/PersonOrderListAdapter.java
@@ -1,9 +1,14 @@
 package com.example.h.checkplz;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.EditText;
 import android.widget.TextView;
+
+import java.util.List;
 
 public class PersonOrderListAdapter extends
         RecyclerView.Adapter<PersonOrderListAdapter.ViewHolder>{
@@ -24,9 +29,51 @@ public class PersonOrderListAdapter extends
             // Stores the itemView in a public final member variable that can be used
             // to access the context from any ViewHolder instance.
             super(itemView);
+            personOrderNameEditText = (EditText) itemView.findViewById(R.id.person_order_name);
+            personOrderCostEditText = (EditText) itemView.findViewById(R.id.person_order_cost);
+            personOrderMultipleSymbolTextView = (TextView) itemView.findViewById(R.id.person_order_multiple_symbol);
+            personOrderMultipleAmountEditText = (EditText) itemView.findViewById(R.id.person_order_multiple_amount);
 
 
         }
     }
 
+    // Store a member variable for the contacts
+    private List<PersonBill> mpersonBillList;
+
+    // Pass in the contact array into the constructor
+    public PersonOrderListAdapter(List<PersonBill> personBillList){
+        mpersonBillList=personBillList;
+    }
+
+    // Usually involves inflating a layout from XML and returning the holder
+    @Override
+    public PersonOrderListAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        Context context = parent.getContext();
+        LayoutInflater inflater = LayoutInflater.from(context);
+
+        // Inflate the custom layout
+        View contactView = inflater.inflate(R.layout.item_person, parent, false);
+
+        // Return a new holder instance
+        PersonOrderListAdapter.ViewHolder viewHolder = new PersonOrderListAdapter.ViewHolder(contactView);
+
+        return viewHolder;
+    }
+
+    // Involves populating data into the item through holder
+    @Override
+    public void onBindViewHolder( PersonOrderListAdapter.ViewHolder viewHolder, int position) {
+        // Get the data model based on position
+        PersonBill personBill = mpersonBillList.get(position);
+
+        // Set item views based on your views and data model
+
+    }
+
+    // Returns the total count of items in the list
+    @Override
+    public int getItemCount() {
+        return mpersonBillList.size();
+    }
 }
