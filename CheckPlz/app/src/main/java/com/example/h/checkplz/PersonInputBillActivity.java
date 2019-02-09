04d7d3c9754a36be52198fbcf5c6c85534f895bf
Jack <gungravefire@yahoo.com>
2019-02-09 17:05:59 -0600

    PersonInputBillActivity updated with onOptionItemSelected

--- CheckPlz/app/src/main/java/com/example/h/checkplz/PersonInputBillActivity.java
+++ CheckPlz/app/src/main/java/com/example/h/checkplz/PersonInputBillActivity.java
@@ -1,9 +1,12 @@
 package com.example.h.checkplz;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
 
 public class PersonInputBillActivity extends AppCompatActivity {
 
@@ -20,5 +23,19 @@ public class PersonInputBillActivity extends AppCompatActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.complete_person_input_bill_activity, menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()){
+            /*Send completed personBill to PeopleBillListActivity*/
+            case R.id.action_done:
+                /*Init PersonBill by setting person's name and fill array of person's order*/
+                Intent intent = new Intent(this, PeopleBillListActivity.class);
+                startActivity(intent);
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
