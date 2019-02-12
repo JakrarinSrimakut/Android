3e7a9169a7ea91be713fda48620c1b0fa9a9b72e
Jack <gungravefire@yahoo.com>
2019-02-11 22:00:35 -0600

    Begin instance PersonBill

--- CheckPlz/app/src/main/java/com/example/h/checkplz/PersonInputBillActivity.java
+++ CheckPlz/app/src/main/java/com/example/h/checkplz/PersonInputBillActivity.java
@@ -5,10 +5,12 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 import android.view.MenuItem;
 
 public class PersonInputBillActivity extends AppCompatActivity {
+    private PersonBill mPersonBill;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,16 +23,19 @@ public class PersonInputBillActivity extends AppCompatActivity {
         /*Init PersonBill to hold person name and person's orders in arraylist
         * only when menu check list is click so as to not have to create then delete
         * user click back button. But how about editing? When you edit and click check
-        * mark is clicked it will generate a new instance.
+        * mark it will generate a new instance. Answer: make a private PersonBill member
+        * variable to set every different PersonBill
         *
         * How to populate PersonBill when it's sent here by intent? Send the PersonBill
-        * here then run methods to fill the activity
+        * here then run methods to fill the activity. Call PersonOrderListAdapter constructor
+        * and send PersonOrderList
         *
         * To grab something. recyclerView.getAdapter().getList().get(position)
          *
          * Pass back PersonBill individually or as a List of Perosn Bill to
          * PeopleBillListActivity? Individual is more efficient. No need to update
-         * entire list when it wasn't edited.
+         * entire list when it wasn't edited. But an PersonOrderListAdapter has
+         * a constructor for taking a list of PersonOrder.
          *
          */
     }
@@ -47,6 +52,10 @@ public class PersonInputBillActivity extends AppCompatActivity {
             /*Send completed personBill to PeopleBillListActivity*/
             case R.id.action_done:
                 /*Init PersonBill by setting person's name and fill array of person's order*/
+                mPersonBill = new PersonBill();
+                mPersonBill.setmName();
+                PersonOrderListAdapter final mAdapter = (PersonOrderListAdapter) RecyclerView.getAdapter().getName();
+
                 Intent intent = new Intent(this, PeopleBillListActivity.class);
                 startActivity(intent);
                 return true;
