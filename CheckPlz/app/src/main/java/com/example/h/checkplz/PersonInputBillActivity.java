329534d44f9395fa23516b53fd6cb8d6cdd2c478
Jack <gungravefire@yahoo.com>
2019-02-19 21:54:45 -0600

    Recyclerview in PersonOrderInputActivity

--- CheckPlz/app/src/main/java/com/example/h/checkplz/PersonInputBillActivity.java
+++ CheckPlz/app/src/main/java/com/example/h/checkplz/PersonInputBillActivity.java
@@ -5,15 +5,20 @@ import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.widget.EditText;
 
+import java.util.ArrayList;
+
 public class PersonInputBillActivity extends AppCompatActivity {
+    private ArrayList<PersonOrder> personOrders = new ArrayList<>();
     private PersonBill mPersonBill;
     private EditText editName;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,8 +27,24 @@ public class PersonInputBillActivity extends AppCompatActivity {
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("PersonInputBillActivity");
 
+        //editName=(EditText) findViewById(R.id.person_name_input_bill_activity);
         //Create recyclerView and adapter to be reference
-
+        // Lookup the recyclerview in activity layout
+        RecyclerView rvPersonOrdersList = (RecyclerView) findViewById(R.id.rv_person_order_list);
+        
+        /*
+        //Initialize Orders
+        PersonOrder pOrder = new PersonOrder();
+        personOrders.add(pOrder);
+        personOrders.add(new PersonOrder());
+        //Create adapter passing the sample user data
+        PersonOrderListAdapter adapter = new PersonOrderListAdapter(personOrders);
+        //Attach the adapter to the recyclerview to populate items
+        rvPersonOrdersList.setAdapter(adapter);
+        //Set Layout manager to position the items
+        rvPersonOrdersList.setLayoutManager(new LinearLayoutManager(this));
+        */
+        
         /*Init PersonBill to hold person name and person's orders in arraylist
         * only when menu check list is click. But how about editing? When you edit and click check
         * mark it will generate a new instance. Answer: make a private PersonBill member
@@ -55,11 +76,11 @@ public class PersonInputBillActivity extends AppCompatActivity {
             /*Send completed personBill to PeopleBillListActivity*/
             case R.id.action_done:
                 /*Init PersonBill by setting person's name and fill array of person's order*/
-                mPersonBill = new PersonBill();
-                mPersonBill.setmName(editName.getText().toString());
+                //mPersonBill = new PersonBill();
+                //mPersonBill.setmName(editName.getText().toString());
                 //PersonOrderListAdapter mAdapter = (PersonOrderListAdapter) RecyclerView.getAdapter().getList();
 
-                mPersonBill.setmPersonOrders((PersonOrderListAdapter) RecyclerView.getAdapter().getList());
+               // mPersonBill.setmPersonOrders((PersonOrderListAdapter) RecyclerView.getAdapter().getList());
 
                 Intent intent = new Intent(this, PeopleBillListActivity.class);
                 startActivity(intent);
