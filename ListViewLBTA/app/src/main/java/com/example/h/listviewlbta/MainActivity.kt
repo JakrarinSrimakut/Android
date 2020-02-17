837f6c1e65b5cf163cfbd6672c3fc785352090a8
Jack <gungravefire@yahoo.com>
2020-02-16 20:43:48 -0600

    Apply converView and ViewHolder to reduce memory usage of findViewById

--- ListViewLBTA/app/src/main/java/com/example/h/listviewlbta/MainActivity.kt
+++ ListViewLBTA/app/src/main/java/com/example/h/listviewlbta/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Context
 import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -22,21 +23,25 @@ class MainActivity : AppCompatActivity() {
 //        val redColor = Color.parseColor("#FF0000")
 //        listView.setBackgroundColor(redColor)
 
-        listView.adapter = MyCustomAdapter(this) // this needs to be my custom adapter telling my list what to render
+        listView.adapter = MyCustomAdapter() // this needs to be my custom adapter telling my list what to render
     }
 
     //Adapter to show listview
-    private class MyCustomAdapter(context: Context): BaseAdapter() {
+    private class MyCustomAdapter(): BaseAdapter() {
 
-        private val mContext: Context
+//        private val mContext: Context
 
         private val names = arrayListOf<String>(
+            "Donald Trump", "Steve Jobs", "Tim Cook", "Mark Zuckerberg", "Barack Obama",
+            "Donald Trump", "Steve Jobs", "Tim Cook", "Mark Zuckerberg", "Barack Obama",
+            "Donald Trump", "Steve Jobs", "Tim Cook", "Mark Zuckerberg", "Barack Obama",
+            "Donald Trump", "Steve Jobs", "Tim Cook", "Mark Zuckerberg", "Barack Obama",
             "Donald Trump", "Steve Jobs", "Tim Cook", "Mark Zuckerberg", "Barack Obama"
         )
 
-        init {
-            mContext = context
-        }
+//        init {
+//            mContext = context
+//        }
         //Responsible for how many rows in my list
         override fun getCount(): Int {
             return names.size
@@ -54,19 +59,36 @@ class MainActivity : AppCompatActivity() {
 
         //Responsible for rendering out each row
         override fun getView(position: Int, convertView: View?, viewGroup: ViewGroup?): View {
-            val layoutInflater = LayoutInflater.from(mContext)
-            val rowMain = layoutInflater.inflate(R.layout.row_main, viewGroup, false)
 
-            val nameTextView = rowMain.findViewById<TextView>(R.id.name_textview)
-            nameTextView.text = names.get(position)
+            val rowMain: View
 
-            val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
-            positionTextView.text = "Row number: $position"
+            // checking if convertView is null, meaning we have to inflate a new row
+            if(convertView == null){
+                val layoutInflater = LayoutInflater.from(viewGroup!!.context)//layout inflater expensive
+                rowMain = layoutInflater.inflate(R.layout.row_main, viewGroup, false)
 
+                Log.v("getView", "calling findViewById which is expensive")
+//                val nTextView = rowMain.name_textview //kotlin allows calling view id w/o using findView also possible cache
+//                val pTextView = rowMain.position_textview
+//                val nameTextView = rowMain.findViewById<TextView>(R.id.name_textview)
+//                val positionTextView = rowMain.findViewById<TextView>(R.id.position_textview)
+                val viewHolder = ViewHolder(rowMain.name_textview, rowMain.position_textview)
+                rowMain.tag = viewHolder
+
+            }else{
+                // well, we have our row as convertView, so just set rowMain as that view
+                rowMain = convertView
+            }
+
+
+            val viewHolder = rowMain.tag as ViewHolder
+            viewHolder.nameTextView.text = names.get(position)
+            viewHolder.positionTextView.text = "Row number: $position"
             return rowMain
-//            val textView = TextView(mContext)
-//            textView.text = "HERE is my ROW for my LISTVIEW"
-//            return textView
+        }
+
+        private class ViewHolder(val nameTextView: TextView, val positionTextView: TextView){
+
         }
 
     }
